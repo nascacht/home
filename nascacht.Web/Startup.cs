7e5d89d37ceda9aa6a926d7a81ee04eb536019bd
epatrick <patrick.eric@gmail.com>
2015-08-19 16:42:35 -0400

    Added web test harness.

---
+++ nascacht.Web/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace nascacht.Web
+{
+    public class Startup
+    {
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Map("/login", login =>
+            {
+                login.Run(async context =>
+                {
+                    await context.Response.WriteAsync("Time to Log in!");
+                });
+            });
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
